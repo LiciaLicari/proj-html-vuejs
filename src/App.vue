770a17ebaf0f121735d7a09fed6b098cbd933093
“LiciaLicari” <licianbsplicari@gmail.com>
2023-10-04 21:36:53 +0200

    add footer_top

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,18 @@ export default {
     <AppCards />
 
     <main>
-        <div id="who_we_are_section" class="p-5 mb-4">
+        <div id="who_we_are_section" class="p-5">
             <div class="container-fluid align-items-center d-flex flex-column py-5">
                 <div class="">
                     <h6>Who We Are</h6>
                     <h1 class="">Sense the jazz</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur et expedita dignissimos? Illum debitis dolores dicta quam doloribus inventore error unde accusantium, architecto vel sapiente quis expedita hic provident omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus illum eveniet iure sapiente, sunt totam, sed earum, soluta minima maxime nihil quibusdam! Quos voluptate eaque impedit eum minima maxime ut. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos omnis, odit facere voluptatibus illo tempora aperiam soluta eaque error, culpa expedita, vel iure dolores sunt porro earum? Eaque, deserunt tempore.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur et expedita dignissimos? Illum
+                        debitis dolores dicta quam doloribus inventore error unde accusantium, architecto vel sapiente quis
+                        expedita hic provident omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus illum
+                        eveniet iure sapiente, sunt totam, sed earum, soluta minima maxime nihil quibusdam! Quos voluptate
+                        eaque impedit eum minima maxime ut. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+                        omnis, odit facere voluptatibus illo tempora aperiam soluta eaque error, culpa expedita, vel iure
+                        dolores sunt porro earum? Eaque, deserunt tempore.</p>
                 </div>
                 <div>
                     <button type="button" class="btn btn_cstm btn-lg">read more</button>
@@ -36,9 +42,42 @@ export default {
 
         </div>
 
+
     </main>
 
-    <footer></footer>
+    <footer>
+        <div class="footer_top d-flex">
+
+            <div class="col">
+                <img src="./assets/img/instagram_img1.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img2.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img3.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img4.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img5.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img6.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img7.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="./assets/img/instagram_img8.jpg" alt="">
+            </div>
+
+        </div>
+
+        <div class="footer_mid"></div>
+        <div class="footer_bot"></div>
+    </footer>
 </template>
 
 <style lang="scss">
